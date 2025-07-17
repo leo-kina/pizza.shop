07826919e9e8b5177ed4e4d9affa338dcd3b11aa
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-07-17 19:29:36 -0300

    adicionado label e input do shadcn e terminado pag de login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Pizza} from 'lucide-react'
 
 export function AuthLayout(){
     return(
-        <div>
-            <h1>Autenticacao</h1>
-            <div><Outlet/></div>
+        <div className="min-h-screen grid grid-cols-2">
+            <div className="h-full border-foreground/5 bg-muted- p-10 flex flex-col bg-muted justify-between">
+            <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+                <Pizza className="h-5 w-5"/>
+                <span className="font-semibold">pizza.shop</span>
+            </div>
+            <footer>Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}</footer>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <Outlet/>
+            </div>
         </div>
     )
 }
