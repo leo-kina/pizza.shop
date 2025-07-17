0b02d6ac45ddce3fcb1f6e3e7a49ad6ed5471939
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-07-16 21:32:48 -0300

    libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-
-
-
+import './global.css'
+import { Button } from "@/components/ui/button"
 export function App() {
 
 
   return (
-
-<h1>hello wordl</h1>
-
+<div className='flex justify-center items-center h-screen'>
+<Button variant="outline">Button</Button>
+</div>
   )
 }
 
