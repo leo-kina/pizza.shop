8d065936917c4316cb5a11b4ec7ddfadb82c6c62
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-07-21 22:57:49 -0300

    fim graficos

--- src/pages/app/dashboard/revenue-char.tsx
+++ src/pages/app/dashboard/revenue-char.tsx
@@ -1,7 +1,17 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
+import {ResponsiveContainer, LineChart, XAxis, YAxis, Line, Tooltip, CartesianGrid} from 'recharts'
 
 export const RevenueChar = () => {
+  const data = [
+    {date: '21/06' , revenue:1200},
+    {date: '22/06' , revenue:200},
+    {date: '23/06' , revenue:100},
+    {date: '24/06' , revenue:2200},
+    {date: '25/06' , revenue:9200},
+    {date: '26/06' , revenue:130},
+    {date: '27/06' , revenue:1550},
+  ]
   return (
     <div>
         <Card className='col-span-4'>
@@ -9,7 +19,18 @@ export const RevenueChar = () => {
                 <CardTitle className='text-base font-medium'>Receita no periodo</CardTitle>
                 <CardDescription>Receita diaria no periodo</CardDescription>
             </CardHeader>
+            <CardContent>
+              <ResponsiveContainer width={"100%"} height={240}>
+                <LineChart style={{fontSize:12}} data={data}>
+                  <XAxis dataKey={'date'} tickLine={false} axisLine={false} dy={16}/>
+                  <YAxis width={80} stroke='#888' axisLine={false} tickLine={false} tickFormatter={(value:number) => value.toLocaleString('pt-BR',{style : 'currency',currency: 'BRL'})}/>
+                  <Line type={'linear'} strokeWidth={2} dataKey={'revenue'} stroke={'#7b45df'}/>
+                </LineChart>
+              </ResponsiveContainer>
+            </CardContent>
+            
         </Card>
+        
     </div>
   )
 }
